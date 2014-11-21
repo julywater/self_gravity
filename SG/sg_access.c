03423c7caeb22111b0688f62035957cb34cf69ed
Yike Tang <yt611@pd232.(none)>
2014-11-21 15:04:01 -0500

    changes in the  struct creation  function

--- SG/sg_access.c
+++ SG/sg_access.c
@@ -1,14 +1,8 @@
-#define SG_PRIVATE_DEFS
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include "../Headers/SG.h"
-#include "../Headers/Cell.h"
-#include "../Headers/Sim.h"
-#include "../Headers/Face.h"
-#include "../Headers/GravMass.h"
-#include "../Headers/header.h"
-#include "../Headers/MPIsetup.h"
+#include "../SG.h"
+#include "../paul.h"
 
 double sg_get_r(struct poisson* thePoisson,int i){
 	int p=thePoisson->r_rank;
